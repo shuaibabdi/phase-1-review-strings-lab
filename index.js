6630b72217a74c8c8af1787e2254263b36814fb6
shuaib abdi <shuaibabdi1015@gmail.com>
2023-10-04 12:33:59 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+
+
 // Write your code in this file!
+let currentUser ="shuaib";
+let welcomeMessage =`Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let shortGreeting =`Welcome, ${currentUser.slice(0, 1)}!`;
